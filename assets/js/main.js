18feb080faa404579d4e14474850e206e17b2d71
AnJaWr <october3@o2.pl>
2021-02-24 22:34:24 +0100

    anim arrows

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,14 +2,18 @@ $(document).ready(function () {
   var start_about = document.getElementById('about-btn'),
     end_about = document.getElementById('about_dot'),
     start_contact = document.getElementById('contact-btn'),
-    end_contact = document.getElementById('contact_dot');
+    end_contact = document.getElementById('contact_dot'),
+    element2 = document.getElementById('two_dot'),
+    element3 = document.getElementById('three_dot'),
+    element4 = document.getElementById('four_dot'),
+    element5 = document.getElementById('five_dot');
 
   var lineAbout = new LeaderLine(start_about, end_about, {
     color: 'rgb(196, 196, 196)',
     size: 1,
     path: 'grid',
     startPlug: 'disc',
-    endPlug: 'disc',
+    endPlug: 'behind',
     startPlugSize: 2,
     startPlugOutlineSize: 2.5,
     endPlugOutlineSize: 1,
@@ -20,35 +24,93 @@ $(document).ready(function () {
     size: 1,
     path: 'grid',
     startPlug: 'disc',
-    endPlug: 'disc',
+    endPlug: 'behind',
     startPlugSize: 2,
     startPlugOutlineSize: 2.5,
     endPlugOutlineSize: 1,
     hide: true,
   });
 
+  // var line1 = new LeaderLine(end_about, element2, {
+  //   color: 'rgba(123, 20, 123, 0.5)',
+  //   size: 1,
+  //   path: 'straight',
+  //   startPlug: 'behind',
+  //   endPlug: 'behind',
+  //   startPlugSize: 2,
+  //   startPlugOutlineSize: 2.5,
+  //   endPlugOutlineSize: 1,
+  //   hide: true,
+  // });
+
+  // var line2 = new LeaderLine(end_about, element3, {
+  //   color: 'rgba(123, 20, 123, 0.5)',
+  //   size: 1,
+  //   path: 'straight',
+  //   startPlug: 'behind',
+  //   endPlug: 'behind',
+  //   startPlugSize: 2,
+  //   startPlugOutlineSize: 2.5,
+  //   endPlugOutlineSize: 1,
+  //   hide: true,
+  // });
+
+  // var line3 = new LeaderLine(element3, element4, {
+  //   color: 'rgba(123, 20, 123, 0.5)',
+  //   size: 1,
+  //   path: 'straight',
+  //   startPlug: 'behind',
+  //   endPlug: 'behind',
+  //   startPlugSize: 2,
+  //   startPlugOutlineSize: 2.5,
+  //   endPlugOutlineSize: 1,
+  //   hide: true,
+  // });
+
+  // var line4 = new LeaderLine(element4, end_contact, {
+  //   color: 'rgba(123, 20, 123, 0.5)',
+  //   size: 1,
+  //   path: 'straight',
+  //   startPlug: 'behind',
+  //   endPlug: 'behind',
+  //   startPlugSize: 2,
+  //   startPlugOutlineSize: 2.5,
+  //   endPlugOutlineSize: 1,
+  //   hide: true,
+  // });
+
+  // var line5 = new LeaderLine(element5, end_contact, {
+  //   color: 'rgba(123, 20, 123, 0.5)',
+  //   size: 1,
+  //   path: 'straight',
+  //   startPlug: 'behind',
+  //   endPlug: 'behind',
+  //   startPlugSize: 2,
+  //   startPlugOutlineSize: 2.5,
+  //   endPlugOutlineSize: 1,
+  //   hide: true,
+  // });
+
   $('.tip').hide();
-  // $('.leader-line').hide();
 
-  // $('#logotyp').on('click', function () {
-  //   $('.tip').fadeIn(1000);
-  //   lineAbout.show('draw', { duration: 300, timing: 'linear' });
-
+  // line4.show('draw', { duration: 6000, timing: 'linear' });
+  // line3.show('draw', { duration: 6000, timing: 'linear' });
+  // line1.show('draw', { duration: 6000, timing: 'linear' });
+  // line2.show('draw', { duration: 6000, timing: 'linear' });
+  // line5.show('draw', { duration: 6000, timing: 'linear' });
   $('#logotyp').on('click', function () {
-    // lineAbout.show('draw', { duration: 300, timing: 'linear' });
-    // $('.leader-line').fadeToggle(1000),
-    lineAbout.show('draw', { duration: 300, timing: 'linear' }),
-      lineContact.show('draw', { duration: 300, timing: 'linear' }),
+    lineAbout.show('draw', { duration: 600, timing: 'linear' }),
+      lineContact.show('draw', { duration: 600, timing: 'linear' }),
       $('.tip').fadeToggle(1000, function () {
         if ($('.tip').is(':hidden')) {
-          // lineAbout.hide('draw', { duration: 400, timing: 'linear' });
           lineAbout.hide();
           lineContact.hide();
         } else {
-          lineAbout.show('draw', { duration: 300, timing: 'linear' });
-          lineContact.show('draw', { duration: 300, timing: 'linear' });
+          lineAbout.show('draw', { duration: 600, timing: 'linear' });
+          lineContact.show('draw', { duration: 600, timing: 'linear' });
         }
       });
+    $('.arrows-container').fadeToggle();
   });
 
   $('.section').hide();
@@ -62,7 +124,4 @@ $(document).ready(function () {
   $('.close').click(function () {
     $('.section').fadeOut(1000);
   });
-
-  // var vid = document.getElementById('video_bg');
-  // vid.playbackRate = 0.5;
 });
