84687963c79838589d5ce42617617fcde6eb7842
AnJaWr <october3@o2.pl>
2021-02-25 23:02:20 +0100

    changes via list

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,15 @@
 $(document).ready(function () {
-  var start_about = document.getElementById('about-btn'),
-    end_about = document.getElementById('about_dot'),
-    start_contact = document.getElementById('contact-btn'),
-    end_contact = document.getElementById('contact_dot'),
-    element2 = document.getElementById('two_dot'),
-    element3 = document.getElementById('three_dot'),
-    element4 = document.getElementById('four_dot'),
-    element5 = document.getElementById('five_dot');
+  var end_about = document.getElementById('about-btn'),
+    start_about = document.getElementById('about_dot'),
+    end_contact = document.getElementById('contact-btn'),
+    start_contact = document.getElementById('contact_dot');
 
   var lineAbout = new LeaderLine(start_about, end_about, {
     color: 'rgb(196, 196, 196)',
     size: 1,
     path: 'grid',
-    startPlug: 'disc',
-    endPlug: 'behind',
+    startPlug: 'behind',
+    endPlug: 'disc',
     startPlugSize: 2,
     startPlugOutlineSize: 2.5,
     endPlugOutlineSize: 1,
@@ -23,111 +19,51 @@ $(document).ready(function () {
     color: 'rgb(196, 196, 196)',
     size: 1,
     path: 'grid',
-    startPlug: 'disc',
-    endPlug: 'behind',
+    startPlug: 'behind',
+    endPlug: 'disc',
     startPlugSize: 2,
     startPlugOutlineSize: 2.5,
     endPlugOutlineSize: 1,
     hide: true,
   });
 
-  // var line1 = new LeaderLine(end_about, element2, {
-  //   color: 'rgba(123, 20, 123, 0.5)',
-  //   size: 1,
-  //   path: 'straight',
-  //   startPlug: 'behind',
-  //   endPlug: 'behind',
-  //   startPlugSize: 2,
-  //   startPlugOutlineSize: 2.5,
-  //   endPlugOutlineSize: 1,
-  //   hide: true,
-  // });
-
-  // var line2 = new LeaderLine(end_about, element3, {
-  //   color: 'rgba(123, 20, 123, 0.5)',
-  //   size: 1,
-  //   path: 'straight',
-  //   startPlug: 'behind',
-  //   endPlug: 'behind',
-  //   startPlugSize: 2,
-  //   startPlugOutlineSize: 2.5,
-  //   endPlugOutlineSize: 1,
-  //   hide: true,
-  // });
-
-  // var line3 = new LeaderLine(element3, element4, {
-  //   color: 'rgba(123, 20, 123, 0.5)',
-  //   size: 1,
-  //   path: 'straight',
-  //   startPlug: 'behind',
-  //   endPlug: 'behind',
-  //   startPlugSize: 2,
-  //   startPlugOutlineSize: 2.5,
-  //   endPlugOutlineSize: 1,
-  //   hide: true,
-  // });
-
-  // var line4 = new LeaderLine(element4, end_contact, {
-  //   color: 'rgba(123, 20, 123, 0.5)',
-  //   size: 1,
-  //   path: 'straight',
-  //   startPlug: 'behind',
-  //   endPlug: 'behind',
-  //   startPlugSize: 2,
-  //   startPlugOutlineSize: 2.5,
-  //   endPlugOutlineSize: 1,
-  //   hide: true,
-  // });
-
-  // var line5 = new LeaderLine(element5, end_contact, {
-  //   color: 'rgba(123, 20, 123, 0.5)',
-  //   size: 1,
-  //   path: 'straight',
-  //   startPlug: 'behind',
-  //   endPlug: 'behind',
-  //   startPlugSize: 2,
-  //   startPlugOutlineSize: 2.5,
-  //   endPlugOutlineSize: 1,
-  //   hide: true,
-  // });
-
+  $('.section').hide();
   $('.tip').hide();
   $('#contact_dot, #about_dot').hide();
 
-  // line4.show('draw', { duration: 6000, timing: 'linear' });
-  // line3.show('draw', { duration: 6000, timing: 'linear' });
-  // line1.show('draw', { duration: 6000, timing: 'linear' });
-  // line2.show('draw', { duration: 6000, timing: 'linear' });
-  // line5.show('draw', { duration: 6000, timing: 'linear' });
   $('#logotyp').on('click', function () {
-    lineAbout.show('draw', { duration: 600, timing: 'linear' }),
-      lineContact.show('draw', { duration: 600, timing: 'linear' }),
-      $('.tip').fadeToggle(1000, function () {
-        if ($('.tip').is(':hidden')) {
-          lineAbout.hide();
-          lineContact.hide();
-        } else {
-          lineAbout.show('draw', { duration: 600, timing: 'linear' });
-          lineContact.show('draw', { duration: 600, timing: 'linear' });
-        }
-      });
+    if ($('.tip').is(':hidden')) {
+      $('.tip').fadeIn();
+      lineAbout.show('draw', { duration: 600, timing: 'linear' }),
+        lineContact.show('draw', { duration: 600, timing: 'linear' });
+      $('.circle-animated').fadeIn();
+    } else {
+      $('.tip').fadeOut();
+      lineAbout.hide();
+      lineContact.hide();
+      $('.circle-animated').fadeOut();
+    }
     $('.arrows-container').fadeToggle();
-    $('#contact_dot , #about_dot').fadeToggle();
   });
 
-  $('.section').hide();
-
   $('.navigation-button').click(function () {
-    $('.section, .tip, .leader-line').hide();
+    $('.section, .tip').hide();
     $('.tip').hide();
-    $('.circle-animated').toggle();
-
-    console.log($('#' + $(this).data('id')));
-    $('#' + $(this).data('id')).fadeIn(1000);
+    $('.circle-animated').hide();
+    lineAbout.hide();
+    lineContact.hide();
+    $('.logotyp svg').hide();
+    if ($(this).is('[class*="contact"]')) {
+      $('#contact').fadeIn(1000);
+    } else {
+      $('#' + $(this).data('id')).fadeIn(1000);
+    }
   });
 
   $('.close').click(function () {
-    $('.section').fadeOut(1000);
+    $('.section').fadeOut(700);
     $('.arrows-container').fadeIn();
+    $('.circle-animated').hide();
+    $('.logotyp svg').show();
   });
 });
