bd2129740b85671bd096a4c8b5f062355901649b
AnJaWr <october3@o2.pl>
2021-02-27 21:17:22 +0100

    divs section changed

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,9 @@ $(document).ready(function () {
   var end_about = document.getElementById('about-btn'),
     start_about = document.getElementById('about_dot'),
     end_contact = document.getElementById('contact-btn'),
-    start_contact = document.getElementById('contact_dot');
+    start_contact = document.getElementById('contact_dot'),
+    start_offer = document.getElementById('offer_dot'),
+    end_offer = document.getElementById('offer_btn');
 
   var lineAbout = new LeaderLine(start_about, end_about, {
     color: 'rgb(196, 196, 196)',
@@ -26,21 +28,34 @@ $(document).ready(function () {
     endPlugOutlineSize: 1,
     hide: true,
   });
+  var lineOffer = new LeaderLine(start_offer, end_offer, {
+    color: 'rgb(196, 196, 196)',
+    size: 1,
+    path: 'grid',
+    startPlug: 'behind',
+    endPlug: 'disc',
+    startPlugSize: 2,
+    startPlugOutlineSize: 2.5,
+    endPlugOutlineSize: 1,
+    hide: true,
+  });
 
   $('.section').hide();
   $('.tip').hide();
-  $('#contact_dot, #about_dot').hide();
+  $('#contact_dot, #about_dot, #offer_dot').hide();
 
   $('#logotyp').on('click', function () {
     if ($('.tip').is(':hidden')) {
       $('.tip').fadeIn();
       lineAbout.show('draw', { duration: 600, timing: 'linear' }),
-        lineContact.show('draw', { duration: 600, timing: 'linear' });
-      $('.circle-animated').fadeIn();
+        lineOffer.show('draw', { duration: 600, timing: 'linear' }),
+        lineContact.show('draw', { duration: 600, timing: 'linear' }),
+        $('.circle-animated').fadeIn();
     } else {
       $('.tip').fadeOut();
       lineAbout.hide();
       lineContact.hide();
+      lineOffer.hide();
       $('.circle-animated').fadeOut();
     }
     $('.arrows-container').fadeToggle();
@@ -53,6 +68,7 @@ $(document).ready(function () {
     $('.circle-animated').hide();
     lineAbout.hide();
     lineContact.hide();
+    lineOffer.hide();
     $('.logotyp svg').hide();
     if ($(this).is('[class*="contact"]')) {
       $('#contact').fadeIn(1000);
@@ -68,6 +84,7 @@ $(document).ready(function () {
     $('.logotyp svg').show();
   });
   $('.animation_quote_container').hide();
+
   $(function () {
     var options = {
       strings: [
